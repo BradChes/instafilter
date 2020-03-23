9925f4068c680d09d7d2cec989d3a489a61bddef
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-03-23 16:31:57 +0000

    Added code to import a picture from photo library

--- instafilter/ViewController.swift
+++ instafilter/ViewController.swift
@@ -13,11 +13,23 @@ class ViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
     
+    var currentImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Instafilter"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
     }
-
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
     @IBAction func changeFilter(_ sender: Any) {
     }
     
@@ -26,5 +38,18 @@ class ViewController: UIViewController {
     
     @IBAction func intensityChanged(_ sender: Any) {
     }
+}
+
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+    }
+
+}
+
+extension ViewController: UINavigationControllerDelegate {
+    
 }
 
